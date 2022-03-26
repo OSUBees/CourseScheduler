71b295288b1a4dd9b17485ef038b4c067502a397
Just-ZJ <dunlike2big@hotmail.com>
2022-03-26 15:26:13 -0400

    added ruby dependedncies and gemfile

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return <div className="bg-light">Hello</div>;
 }
 
 export default App;
