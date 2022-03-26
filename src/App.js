6df6705e9b070e0286d7d0be5619999e16fb187a
Just-ZJ <dunlike2big@hotmail.com>
2022-03-26 18:52:37 -0400

    added firebase database integrations

--- src/App.js
+++ src/App.js
@@ -1,8 +1,57 @@
 import "./App.css";
+import React, { Component } from "react";
+import { getDatabase, ref, onValue } from "firebase/database";
+import Courses from "./components/courses";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return <div className="bg-light">Hello</div>;
+class App extends Component {
+  state = {
+    semesters: [],
+    courses: [],
+    professors: [],
+  };
+
+  getSemesters() {
+    const dbRef = ref(getDatabase(), "semesters");
+    onValue(dbRef, (snapshot) => {
+      this.setState({ semesters: snapshot.val() });
+    });
+  }
+  getCourses() {
+    const dbRef = ref(getDatabase(), "courses");
+    onValue(dbRef, (snapshot) => {
+      const courses = snapshot.val();
+      this.setState({ courses });
+    });
+  }
+  getProfessors() {
+    const dbRef = ref(getDatabase(), "professors");
+    onValue(dbRef, (snapshot) => {
+      const professors = snapshot.val();
+      this.setState({ professors });
+    });
+  }
+
+  componentDidMount() {
+    console.log("mounted");
+    this.getCourses();
+    this.getSemesters();
+    this.getProfessors();
+  }
+
+  render() {
+    const { courses, professors, semesters } = this.state;
+    return (
+      <div>
+        <SomeComponent
+          semesters={semesters}
+          courses={courses}
+          professors={professors}
+          getSemesters={this.getSemesters}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
