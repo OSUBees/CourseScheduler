fb71bc473ded3779164ef23a7319de579fd71f9e
Just-ZJ <dunlike2big@hotmail.com>
2022-03-26 19:03:42 -0400

    combined files

--- src/App.js
+++ src/App.js
@@ -1,39 +1,35 @@
-// import Prerequisites from "./components/Prerequisites/Prerequisites";
-// import './App.css';
-// import "bootstrap/dist/css/bootstrap.min.css";
-// //temp 数据
-// const class1 =
-//   {
-//     id: "c1",
-//     name: "CSE2221",
-//     description: "Software 1",
-//     credit: 4,
-// }
-// const class2 =
-//   {
-//     id: "c2",
-//     name: "CSE2231",
-//     description: "Software 2",
-//     credit: 3,
-// }
-// const class3 =
-// {
-//   id: "c3",
-//   name: "CSE2321",
-//   description: "Foundation 1",
-//   credit: 3,
-// }
-// function App() {
-//   return (
-
-//       <Prerequisites/>
-
-//   );
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Semester from "./components/Semester/Semester.js";
+import Prerequisites from "./components/Prerequisites/Prerequisites";
+
+//temp 数据
+const class1 = {
+  id: "c1",
+  name: "CSE2221",
+  description: "Software 1",
+  credit: 4,
+};
+const class2 = {
+  id: "c2",
+  name: "CSE2231",
+  description: "Software 2",
+  credit: 3,
+};
+const class3 = {
+  id: "c3",
+  name: "CSE2321",
+  description: "Foundation 1",
+  credit: 3,
+};
 
 function App() {
-  return <Semester />;
+  return (
+    <div>
+      <Semester />
+      <Prerequisites />
+    </div>
+  );
 }
 
 export default App;
