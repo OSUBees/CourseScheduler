a78dc8ab58beb5a99870a36faddc8adcc5b9fe5d
Just-ZJ <dunlike2big@hotmail.com>
2022-03-27 01:04:25 -0400

    removed courses from semesters

--- src/components/Semester/Semester.js
+++ src/components/Semester/Semester.js
@@ -1,50 +1,56 @@
 import CourseItem from "../CourseItem/CourseItem.js";
 import classes from "./Semester.module.css";
+import React, { Component } from "react";
 
-const DUMMY_COURSES = [
-  {
-    id: "c1",
-    name: "CSE2221",
-    description: "Software 1",
-    credit: 4,
-  },
-  {
-    id: "c2",
-    name: "CSE2231",
-    description: "Software 2",
-    credit: 3,
-  },
-  {
-    id: "c3",
-    name: "CSE2321",
-    description: "Foundation 1",
-    credit: 3,
-  },
-  {
-    id: "c4",
-    name: "CSE2331",
-    description: "Foundation 2",
-    credit: 3,
-  },
-];
-
-const Semester = () => {
-  const coursesList = DUMMY_COURSES.map((course) => (
-    <CourseItem
-      id={course.id}
-      name={course.name}
-      description={course.description}
-      credit={course.credit}
-    />
-  ));
-
-  return (
-    <div className={classes.card + " w-25" + classes.block}>
-      <div>Semester X</div>
-      <div className={classes.courses + "w-auto"}>{coursesList}</div>
-      <div>Total Credit:XX</div>
-    </div>
-  );
-};
+class Semester extends Component {
+  state = {};
+  render() {
+    return (
+      <div className={classes.card + " w-25" + classes.block}>
+        <div>Semester X</div>
+        <div className={classes.courses + "w-auto"}></div>
+        <div>Total Credit:XX</div>
+      </div>
+    );
+  }
+}
 
 export default Semester;
+
+// const DUMMY_COURSES = [
+//   {
+//     id: "c1",
+//     name: "CSE2221",
+//     description: "Software 1",
+//     credit: 4,
+//   },
+//   {
+//     id: "c2",
+//     name: "CSE2231",
+//     description: "Software 2",
+//     credit: 3,
+//   },
+//   {
+//     id: "c3",
+//     name: "CSE2321",
+//     description: "Foundation 1",
+//     credit: 3,
+//   },
+//   {
+//     id: "c4",
+//     name: "CSE2331",
+//     description: "Foundation 2",
+//     credit: 3,
+//   },
+// ];
+// const Semester = () => {
+//   const coursesList = DUMMY_COURSES.map((course) => (
+//     <CourseItem
+//       id={course.id}
+//       name={course.name}
+//       description={course.description}
+//       credit={course.credit}
+//     />
+//   ));
+
+// };
